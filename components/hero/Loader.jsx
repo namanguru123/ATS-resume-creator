07428c0c854e29364dcec4afd084bf69b382f2f7
namanguru123 <namansharma7898@gmail.com>
2025-04-05 15:58:33 +0530

    Initial commit

---
+++ components/hero/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className=''> 
+        <h2>
+            Loading 
+        </h2>
+
+        <div classname="loader"></div>
+    </div>
+  )
+}
+ 
+export default Loader
